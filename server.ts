9332666db4d8d554de979783f4f0b7fd3697e0b8
Anton Kalkou <Anton.Kalkou@gmail.com>
2020-06-22 12:51:23 +0300

    Initial server setup

---
+++ server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('combined'));
+
+export default app;
