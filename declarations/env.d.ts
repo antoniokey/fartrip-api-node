4555970fc12c3b90be45df94e0bb3330fd79dc88
Anton Kalkou <Anton.Kalkou@gmail.com>
2020-06-29 17:08:15 +0300

    Added emvironment declaration file

---
+++ declarations/env.d.ts
@@ -0,0 +1,19 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DEV_ORIGIN: string;
+    DB_NAME: string;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
+    DB_PORT: string;
+    DB_HOST: string;
+    DB_DIALECT: string;
+    DB_ACQUIRE: string;
+    DB_IDLE: string;
+    DB_POOL_MAX: string;
+    DB_POOL_MIN: string;
+    DB_POOL_EVICT: string;
+    JWT_SECRET: string;
+    CLIENT_ID: string;
+    CLIENT_SECRET: string;
+  }
+}
